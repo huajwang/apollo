319516c20e485e5450ef8c23ee1fe9827f288f38
Huajian Wang <yc_whj@hotmail.com>
2024-11-09 16:11:48 -0500

    Use env variable or separate yml to store sensitive data

--- nightgrass/src/main/java/com/goodfeel/nightgrass/web/CheckoutController.java
+++ nightgrass/src/main/java/com/goodfeel/nightgrass/web/CheckoutController.java
@@ -1,5 +1,6 @@
 package com.goodfeel.nightgrass.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,14 @@ import reactor.core.publisher.Mono;
 @RequestMapping(path = "/checkout")
 public class CheckoutController {
 
+    @Value("${STRIPE_PUBLIC_KEY}")
+    private String stripePublicKey;
+
+
     @GetMapping
     public Mono<String> checkout(Model model) {
         model.addAttribute("STRIPE_PUBLIC_KEY",
-                "");
+                stripePublicKey);
         return Mono.just("checkout");
     }
 }

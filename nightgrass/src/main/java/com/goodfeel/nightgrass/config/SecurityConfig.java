f15a0c822dec09bf2075c9bd9b8ba20558649a2c
Huajian Wang <yc_whj@hotmail.com>
2024-11-10 12:44:34 -0500

    Configure server to use SSL and redirect all HTTP to HTTPS

--- nightgrass/src/main/java/com/goodfeel/nightgrass/config/SecurityConfig.java
+++ nightgrass/src/main/java/com/goodfeel/nightgrass/config/SecurityConfig.java
@@ -2,10 +2,17 @@ package com.goodfeel.nightgrass.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,7 +22,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/", "/product/**",
+                        .pathMatchers("/", "/product/**", "/cart/**",
                                 "/login**", "/error",
                                 "/images/**", "/css/**", "/webjars/**").permitAll()
                         .anyExchange().authenticated()
@@ -26,4 +33,20 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+//    // Custom WebFilter to redirect HTTP to HTTPS
+//    @Bean
+//    public WebFilter httpsRedirectFilter() {
+//        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+//            if ("http".equals(exchange.getRequest().getURI().getScheme())) {
+//                URI httpsUri =
+//                        URI.create(exchange.getRequest().getURI().toString().replace("http", "https"));
+//                exchange.getResponse().setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+//                exchange.getResponse().getHeaders().setLocation(httpsUri);
+//                return exchange.getResponse().setComplete();
+//            }
+//            return chain.filter(exchange);
+//        };
+//    }
+
 }

f40282e0885d3039ff1a95bfb7714dde1b9f4b54
Huajian Wang <yc_whj@hotmail.com>
2024-12-27 15:53:29 -0500

    Update call back endpoint

--- nightgrass/src/main/kotlin/com/goodfeel/nightgrass/util/Constant.kt
+++ nightgrass/src/main/kotlin/com/goodfeel/nightgrass/util/Constant.kt
@@ -1,6 +1,6 @@
 package com.goodfeel.nightgrass.util
 
 object Constant {
-    const val SERVER_BASE_URL = "http://localhost:8080"
+    const val SERVER_BASE_URL = "https://yaojiabuy.com"
     const val GUEST = "Guest"
 }

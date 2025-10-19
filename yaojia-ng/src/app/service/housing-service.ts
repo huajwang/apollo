40f883b7f22c3517b79a4be414f7ff0ab473f2b7
Huajian Wang <yc_whj@hotmail.com>
2025-10-19 18:16:45 -0400

    Add HTTP communication

--- yaojia-ng/src/app/service/housing-service.ts
+++ yaojia-ng/src/app/service/housing-service.ts
@@ -5,6 +5,9 @@ import { HousingLocationInfo } from '../housinglocation';
   providedIn: 'root'
 })
 export class HousingService {
+
+  readonly url = 'http://localhost:8080/api';
+
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
     housingLocationList: HousingLocationInfo[] = [
       {
@@ -110,11 +113,23 @@ export class HousingService {
     ];
 
   getAllHousingLocations(): HousingLocationInfo[] {
-      return this.housingLocationList;
+    this.getAllHousingLocations2();
+    return this.housingLocationList;
   }
 
   getHousingLocationById(id: number): HousingLocationInfo | undefined {
       return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+
+  async getAllHousingLocations2(): Promise<HousingLocationInfo[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
+  }
+
+  async getHousingLocationById2(id: number): Promise<HousingLocationInfo | undefined> {
+    const data = await fetch(`${this.url}?id=${id}`);
+    const locationJson = await data.json();
+    return locationJson[0] ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {

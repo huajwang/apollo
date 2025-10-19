4c4016e5decfb954d9bad73db57814559b708325
Huajian Wang <yc_whj@hotmail.com>
2025-10-19 17:24:19 -0400

    Add form

--- yaojia-ng/src/app/product/detail/detail.ts
+++ yaojia-ng/src/app/product/detail/detail.ts
@@ -1,11 +1,12 @@
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { HousingService } from '../../service/housing-service';
 import { HousingLocationInfo } from '../../housinglocation';
 
 @Component({
   selector: 'product-detail',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './detail.html',
   styleUrl: './detail.scss'
 })
@@ -13,6 +14,21 @@ export class Detail {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocationInfo | undefined;
+
+  applyForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+    });
+
+  submitApplication() {
+      this.housingService.submitApplication(
+        this.applyForm.value.firstName ?? '',
+        this.applyForm.value.lastName ?? '',
+        this.applyForm.value.email ?? '',
+      );
+    }
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
      this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);

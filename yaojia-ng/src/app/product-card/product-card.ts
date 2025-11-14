2ad6ffe8e47cb90798e526a1a9eae1da75ccb0df
Huajian Wang <yc_whj@hotmail.com>
2025-11-13 20:43:03 -0500

    Use Angular Material

--- yaojia-ng/src/app/product-card/product-card.ts
+++ yaojia-ng/src/app/product-card/product-card.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../model/product';
 import { RouterLink } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, MatCardModule, MatButtonModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss'
 })

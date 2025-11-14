6cbfcda0c73456b2ebd070cc05c743ce45fb2edf
Huajian Wang <yc_whj@hotmail.com>
2025-11-13 21:34:58 -0500

    Add toolbar

--- yaojia-ng/src/app/app.ts
+++ yaojia-ng/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { Home } from './home/home';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- yaojia-ng/src/app/product-card/product-card.ts
+++ yaojia-ng/src/app/product-card/product-card.ts
@@ -3,10 +3,11 @@ import { Product } from '../model/product';
 import { RouterLink } from "@angular/router";
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink, MatCardModule, MatButtonModule],
+  imports: [RouterLink, MatCardModule, MatButtonModule, CurrencyPipe],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss'
 })

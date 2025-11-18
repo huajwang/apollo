93a965da05664cacabf2df046527e6972d77a376
Huajian Wang <yc_whj@hotmail.com>
2025-11-17 20:38:10 -0500

    Clean server side code

--- nightgrass/src/main/kotlin/com/goodfeel/nightgrass/web/util/CartItemUpdateRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.goodfeel.nightgrass.web.util
-
-data class CartItemUpdateRequest(
-    val itemId: Long,
-    val quantity: Int? = null,
-    // the below is for check box
-    val isChecked: Boolean? = null
-)
--- nightgrass/src/main/kotlin/com/goodfeel/nightgrass/web/util/RemoveCartRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.goodfeel.nightgrass.web.util
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class RemoveCartRequest @JsonCreator constructor(
-    @JsonProperty("itemId") val itemId: Long
-)

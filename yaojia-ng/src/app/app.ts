1379ec0f17e06c5c287812b7bae7d274e9baef27
Huajian Wang <yc_whj@hotmail.com>
2025-11-25 20:58:42 -0500

    Styling menu items

--- yaojia-ng/src/app/app.ts
+++ yaojia-ng/src/app/app.ts
@@ -7,6 +7,7 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatDividerModule} from '@angular/material/divider';
 import { CartStore } from './cart/cart-store';
+import { AuthService } from './service/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -17,8 +18,11 @@ import { CartStore } from './cart/cart-store';
 export class App {
   protected readonly title = signal('yaojia-ng');
   cartStore = inject(CartStore);
+  authService = inject(AuthService);
   
   cartItemCount = computed(() => {
     return this.cartStore.cartItems().reduce((sum, item) => sum + item.quantity, 0);
   });
+
+  isLoggedIn = computed(() => this.authService.isLoggedIn());
 }
